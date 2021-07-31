7719f6aa37bf351b76bb72f16bfc3b5f17f26220
Vladimir-Zel <vladimir.zelenov00@gmail.com>
2021-07-31 12:58:04 +0300

    First commit

---
+++ src/main/java/telran/java38/student/dao/StudentMongoRepository.java
@@ -0,0 +1,21 @@
+package telran.java38.student.dao;
+
+import java.util.stream.Stream;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import telran.java38.student.model.Student;
+
+public interface StudentMongoRepository extends MongoRepository<Student, Integer> {
+
+	Stream<Student> findByGroup(String group);
+
+	Stream<Student> findByGroupAndName(String group, String name);
+	
+	long countByGroup(String group);
+	
+	//@Query("{'scores.Math':{'$gte':90}}")
+	@Query("{'?0':{'$gte':?1}}")
+	Stream <Student> findByExamMinScore(String exam, int score);
+}
